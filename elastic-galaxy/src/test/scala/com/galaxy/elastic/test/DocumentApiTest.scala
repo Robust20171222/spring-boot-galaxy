ddcca6c41d6e811499631a646a18e8b97cf77914
jack sparrow <1755610380@qq.com>
2018-05-09 19:54:33 +0800

    add highlight function

--- elastic-galaxy/src/test/scala/com/galaxy/elastic/test/DocumentApiTest.scala
+++ elastic-galaxy/src/test/scala/com/galaxy/elastic/test/DocumentApiTest.scala
@@ -4,6 +4,9 @@ import java.util
 import java.util.Date
 
 import org.elasticsearch.action.get.GetResponse
+import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.index.query.{QueryBuilder, QueryBuilders}
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder
 import org.junit.Test
 
 import scala.io.Source
@@ -11,9 +14,9 @@ import scala.io.Source
 class DocumentApiTest extends BaseTest {
 
   def putJsonDocument(title: String, content: String, postDate: Date, tags: Array[String], author: String): java.util.Map[String, Object] = {
-    val jsonDocument: java.util.Map[String,Object] = new util.HashMap[String,Object]
+    val jsonDocument: java.util.Map[String, Object] = new util.HashMap[String, Object]
     jsonDocument.put("title", title)
-    jsonDocument.put("conten", content)
+    jsonDocument.put("content", content)
     jsonDocument.put("postDate", postDate)
     jsonDocument.put("tags", tags)
     jsonDocument.put("author", author)
@@ -22,7 +25,7 @@ class DocumentApiTest extends BaseTest {
 
   @Test
   def testCreateIndex: Unit = {
-    val doc:java.util.Map[String,Object] = putJsonDocument("ElasticSearch: Java API", "ElasticSearch provides the Java API, all operations "
+    val doc: java.util.Map[String, Object] = putJsonDocument("ElasticSearch: Java API", "ElasticSearch provides the Java API, all operations "
       + "can be executed asynchronously using a client object.", new Date, Array("elasticsearch"), "Hüseyin Akdoğan")
 
     val response = this.transportClient.prepareIndex("kodcucom", "article", "1").setSource(doc).execute().actionGet()
@@ -30,8 +33,8 @@ class DocumentApiTest extends BaseTest {
   }
 
   @Test
-  def testGetIndex: Unit ={
-    val getResponse:GetResponse = this.transportClient.prepareGet("kodcucom", "article", "1").execute.actionGet
+  def testGetIndex: Unit = {
+    val getResponse: GetResponse = this.transportClient.prepareGet("kodcucom", "article", "1").execute.actionGet
 
     val source = getResponse.getSource
     println("------------------------------")
@@ -43,8 +46,29 @@ class DocumentApiTest extends BaseTest {
     println("------------------------------")
   }
 
+  /**
+    * 搜索高亮测试
+    */
   @Test
-  def testHttp: Unit ={
+  def testSearchIndexWithHighlight: Unit = {
+    val indexName = "bookdb_index"
+    val indexType = "book"
+
+    val highlightBuilder: HighlightBuilder = new HighlightBuilder().highlighterType("plain").field("*")
+    val plainResponse: SearchResponse = this.transportClient.prepareSearch(indexName).setTypes(indexType).setQuery(QueryBuilders.matchQuery("title","elasticsearch")).highlighter(highlightBuilder).get()
+    log.info(s"PlainResponse: ${plainResponse}")
+
+    val highlightBuilderWithFvh = new HighlightBuilder().highlighterType("fvh").field("*")
+    val fvhResponse: SearchResponse = this.transportClient.prepareSearch(indexName).setTypes(indexType).setQuery(QueryBuilders.matchQuery("title","elasticsearch guide")).highlighter(highlightBuilderWithFvh).get()
+    log.info(s"FvhResponse: ${fvhResponse}")
+
+    val highlightBuilderWithPostings = new HighlightBuilder().highlighterType("postings").field("*")
+    val postingsResponse: SearchResponse = this.transportClient.prepareSearch(indexName).setTypes(indexType).setQuery(QueryBuilders.matchQuery("summary","elasticsearch guide")).highlighter(highlightBuilderWithPostings).get()
+    log.info(s"PostingsResponse: ${postingsResponse}")
+  }
+
+  @Test
+  def testHttp: Unit = {
     println(Source.fromURL("http://127.0.0.1:9200", "utf-8").getLines.mkString("\n"))
   }
 }
