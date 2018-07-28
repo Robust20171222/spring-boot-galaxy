4283fcecfbaa82b6747414c14abfd8268bb86508
dapeng <1755610380@qq.com>
2018-07-28 15:58:31 +0800

    Remove simple-galaxy

--- simple-galaxy/kafka_start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties > /dev/null 2>&1 &
-sleep 2
-nohup bin/kafka-server-start.sh config/server.properties > /dev/null 2>&1 &
-sleep 2
