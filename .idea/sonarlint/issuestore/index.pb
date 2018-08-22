6cfe14ad686e7fdd7768a9dae62b38700e25ea0f
dapeng <1755610380@qq.com>
2018-08-22 21:50:21 +0800

    Modidy zuul gateway config

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,12 @@ a
 1gateway-galaxy/src/main/resources/application.yml,0/9/0926c4306ba9586bc17ac345194b6df224dd4555
 F
 gateway-galaxy/pom.xml,e/6/e652634a796cb5856005d71f2224de73909e8b17
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+]
+-scala-galaxy/src/main/resources/bootstrap.yml,f/c/fc22f36acdb784ed9e2a720dd1e8008e3028c248
+a
+1scala-galaxy/src/main/resources/bootstrap-dev.yml,1/f/1f1ca00790708470c800fbc52a3fb9613cc514cc
+m
+=scala-galaxy/src/test/scala/com/galaxy/test/TestExample.scala,f/e/fe391d093445eab14f865c69d931c07662749be2
--- scala-galaxy/src/test/scala/com/galaxy/test/TestExample.scala
+++ scala-galaxy/src/test/scala/com/galaxy/test/TestExample.scala
@@ -1,17 +1,19 @@
 package com.galaxy.test
 
+import com.zaxxer.hikari.HikariDataSource
 import org.junit.Test
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
 import org.springframework.boot.env.YamlPropertySourceLoader
+import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean
 
 /**
   * Created by wangpeng on 19/03/2018. 
   */
 class TestExample {
-
-  @Test
-  def test: Unit = {
-    val yml: YamlPropertiesFactoryBean = null
-    val yml2 : YamlPropertySourceLoader = null
-  }
+  //
+  //  @Test
+  //  def test: Unit = {
+  //    HikariDataSource
+  //    AbstractEntityManagerFactoryBean
+  //  }
 }
