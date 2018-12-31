b1a3154b9108d5949cca303b86f6e6be7910f748
dapeng <1755610380@qq.com>
2018-12-31 10:53:18 +0800

    Add filterNot test

--- scala-galaxy/src/main/scala/com/galaxy/scala/web/HotelController.scala
+++ scala-galaxy/src/main/scala/com/galaxy/scala/web/HotelController.scala
@@ -2,9 +2,11 @@ package com.galaxy.scala.web
 
 import com.galaxy.scala.entity.Hotel
 import com.galaxy.scala.service.impl.HotelServiceImpl
+import javax.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation._
+import scala.collection.JavaConverters._
 
 /**
   * Created by wangpeng on 16/03/2018. 
@@ -19,5 +21,14 @@ class HotelController {
   @GetMapping(value = Array("/list"))
   def findAll(): java.lang.Iterable[Hotel] = {
     this.hotelService.findAll()
+  }
+
+  @GetMapping(value = Array("/para"))
+  def testParameter(request: HttpServletRequest,jobTag: String): String ={
+    val allParameters = request.getParameterMap.asScala.toMap
+    val parameterOtherTable = allParameters.filterNot(_._1.startsWith(jobTag))
+      .map(para => para._1 + "=" + para._2(0))
+    println(parameterOtherTable)
+    jobTag
   }
 }
--- scala-galaxy/src/test/scala/com/galaxy/test/colletions/MapTest.scala
+++ scala-galaxy/src/test/scala/com/galaxy/test/colletions/MapTest.scala
@@ -4,13 +4,13 @@ import org.junit.Test
 
 /**
   * Created by wangpeng
-  * Date: 2018/5/21
-  * Time: 21:54
+  * Date: 2018-12-28
+  * Time: 18:18
   */
-class Collection2Test {
+class MapTest {
 
   @Test
-  def test: Unit = {
-    println(409 % 10)
+  def testForeach: Unit ={
+
   }
 }
