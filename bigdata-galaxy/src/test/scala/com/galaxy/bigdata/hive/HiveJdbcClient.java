278d526e06603c2d6af12078e63e6c7e31d1284c
大鹏 <pengwang@groots-MacBook-Pro.local>
2019-05-20 21:59:13 +0800

    Add hiveserver2 jdbc test

---
+++ bigdata-galaxy/src/test/scala/com/galaxy/bigdata/hive/HiveJdbcClient.java
@@ -0,0 +1,46 @@
+package com.galaxy.bigdata.hive;
+
+import java.sql.*;
+
+/**
+ * @author pengwang
+ * @date 2019/05/20
+ */
+public class HiveJdbcClient {
+
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+    /**
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(String[] args) throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
+        }
+        //replace "hive" here with the name of the user the queries should run as
+        Connection con = DriverManager.getConnection("jdbc:hive2://10.104.132.194:10000/ymq", "hadoop", "");
+        Statement stmt = con.createStatement();
+        String tableName = "test";
+
+        // select * query
+        String sql = "select * from " + tableName;
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
+        }
+
+        // regular hive query
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }
+}
